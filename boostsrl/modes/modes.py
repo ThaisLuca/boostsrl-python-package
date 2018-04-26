d29df4decd74558c433d602eadf3933844a34b67
batflyer <alexander@batflyer.net>
2018-04-26 14:08:37 -0500

    Moved modes into its own directory and added metadata and licensing information.

---
+++ boostsrl/modes/modes.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2017-2018 StARLinG Lab
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# See <http://www.gnu.org/licenses/> or the license in the base of
+# the repository at <https://github.com/starling-lab/boostsrl-python-package>
+
+from __future__ import print_function
+
+__author__ = 'Alexander L. Hayes (@batflyer)'
+__copyright__ = 'Copyright (c) 2017-2018 StARLinG Lab'
+__license__ = 'GPLv3'
+__version__ = '1.0.1'
+__maintainer__ = __author__
+__email__ = 'alexander@batflyer.net'
+__status__ = 'Production'
