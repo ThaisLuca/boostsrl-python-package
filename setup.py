1f4838f88cd4a104bc5d921ede6b6735950ba4da
rodrigoazs <rodrigo4zevedo@gmail.com>
2018-07-23 11:58:07 -0300

    Remove train_bk and test_bk from setup

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ setup(
     # boostsrl_java stores files in the user's home directory by default.
     include_package_data = True,
     data_files=[(path.expanduser('~') + '/.boostsrl_data', ['boostsrl/v1-0.jar',
-                                                            'boostsrl/auc.jar']),
-                (path.expanduser('~') + '/.boostsrl_data/train', ['boostsrl/train/train_bk.txt']),
-                (path.expanduser('~') + '/.boostsrl_data/test', ['boostsrl/test/test_bk.txt'])],
+                                                            'boostsrl/auc.jar']),]
+                #(path.expanduser('~') + '/.boostsrl_data/train', ['boostsrl/train/train_bk.txt']),
+                #(path.expanduser('~') + '/.boostsrl_data/test', ['boostsrl/test/test_bk.txt'])],
 
     # Project's main homepage.
     url='https://github.com/batflyer/boostsrl-python-package',
