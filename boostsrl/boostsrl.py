7fabb46262dd6531e9d9e51ae9edd6d8bbb21ee5
Thais Luca <thais.luca95@gmail.com>
2020-10-24 16:22:17 -0300

    Subindo teste de path

--- boostsrl/boostsrl.py
+++ boostsrl/boostsrl.py
@@ -66,7 +66,6 @@ def call_process(call):
     '''Create a subprocess and wait for it to finish. Error out if errors occur.'''
     try:
         p = subprocess.Popen(call, shell=True)
-        p.communicate() 
         os.waitpid(p.pid, 0)
     except:
         raise(Exception('Encountered problems while running process: ', call))
@@ -243,8 +242,9 @@ class train(object):
         write_to_file(self.train_facts, 'boostsrl/train/train_facts.txt')
         
         combine = '' #'-combine ' if self.trees > 1 else ''
+        path = os.getcwd() + '/boostsrl'
         
-        CALL = '(cd boostsrl; java -jar v1-0.jar -l ' + ('-refine refine.txt ' if refine else '') + ('-transfer transfer.txt ' if transfer else '') + combine + '-train train/ -target ' + ','.join(self.target) + \
+        CALL = '(cd ' + path + '; java -jar v1-0.jar -l ' + ('-refine refine.txt ' if refine else '') + ('-transfer transfer.txt ' if transfer else '') + combine + '-train train/ -target ' + ','.join(self.target) + \
                ' -trees ' + str(self.trees) + ' > train_output.txt 2>&1)'
         call_process(CALL)
 
